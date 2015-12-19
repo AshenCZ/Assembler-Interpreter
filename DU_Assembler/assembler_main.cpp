b4568fcee865468b26a3a4be5ea005be87594f49
Ashen <ashencz@gmail.com>
2015-12-19 23:35:14 +0100

    Zacnuti parsovani a prekompilace zdrojaku

--- DU_Assembler/assembler_main.cpp
+++ DU_Assembler/assembler_main.cpp
@@ -39,26 +39,210 @@ class file_resource
 		}
 };
 
-instruction parse_line(string line)
+enum parsing_state { first_word , command_load , arg_before_eq , args_after_eq , skip_space};
+
+void line_cannot_be_parsed(string message)
 {
-	cout << "ParseLine - Doing : " << line << endl;
-	instr_JMP parsed("nav1",1);
+	cout << message << " Exit." << endl;
+}
+void wrong_arguments(string instr, int num, int line_number)
+{
+	string a = "On line " + to_string(line_number);
+	a += ", invalid number of arguments (" + to_string(num) + ") for " + instr + ".";
+
+	line_cannot_be_parsed(a);
+}
+
+instruction parse_line(string line, int line_number)
+{
+	cout << "\nParseLine - Doing : " << line << endl;
+	parsing_state state = first_word;
+	//parsing_state future_state = first_word;
+	string word = "";
+
+	string navesti = "";
+	string cmd = "";
+
+	string words[20];
+	int word_count = 0;
 	
+	//Tokenizing
+	for (size_t i = 0; i < line.length(); i++)
+	{
+		if (line[i] == ' ' && word.length() == 0) // no more spaces, we enter the next mode
+		{
+			continue;
+		}
+		else if (line[i] == ' ' && word.length() != 0) // no more spaces, we enter the next mode
+		{
+			words[word_count] = word;
+			word = "";
+			word_count++;			
+		}
+		else if (line[i] == ',' && word.length() != 0) // no more spaces, we enter the next mode
+		{
+			words[word_count] = word;
+			word = "";
+			word_count++;
+		}
+		else if (line[i] == '=' && word.length() != 0) // no more spaces, we enter the next mode
+		{
+			words[word_count] = word;
+			word = "";
+			word_count++;
+		}
+		else if (line[i] == '=' && word.length() == 0) // no more spaces, we enter the next mode
+		{
+			continue;
+		}
+		else if (line[i] == ',' && word.length() == 0) // no more spaces, we enter the next mode
+		{
+			continue;
+		}
+		else
+		{
+			word += line[i];
+		}
+
+		/*
+		if (line[i] == ':' && state == first_word) // mame navesti
+		{
+			navesti = word;
+			word = "";
+			state = skip_space;
+			future_state = command_load;
+		}
+		else if (line[i] == ':' && state != first_word) // nasli sme syntax error, ":" je nekde jinde
+		{
+			line_cannot_be_parsed("Found : ne na miste navesti! " + line.substr(i-2,5) );
+		}
+		else if (line[i] == ' ' && state == first_word) // end of word
+		{
+			cmd = word;
+			word = "";
+			state = skip_space;
+			future_state = arg_before_eq;
+		}
+		else if (line[i] == ' ' && state == skip_space) // we skip spaces
+		{
+			continue;
+		}
+		else if (line[i] != ' ' && state == skip_space) // no more spaces, we enter the next mode
+		{
+			word += line[i];
+			state = future_state;
+			future_state = skip_space;
+		}
+		else if (line[i] == '=' && state == skip_space) // no more spaces, we enter the next mode
+		{
+			word += line[i];
+			state = future_state;
+			future_state = skip_space;
+		}*/
+	}
+	if (word != "")
+	{
+		words[word_count] = word;
+		word_count++;
+	}
+
+	// TOKENIZED at this point.
+
+	// test
+	for (size_t i = 0; i < word_count; i++)
+	{
+		cout << "'" << words[i] << "'" << endl;
+	}
+	
+	// Begin to recognize the tokens!
+	string command_conditionless = "";
+	string condition = "";
+	int index = 0;
+
+	if (words[0].back() == ':') // navesti
+	{
+		navesti = words[0].substr(0,words[0].length()-1);
+		cout << "Navesti: '" << navesti << "'" << endl;
+		index = 1;
+	}
+
+	size_t dot = words[index].find('.');
+	if (dot != string::npos)
+	{
+		condition = words[index].substr(dot+1, words[index].length() - dot);
+		command_conditionless = words[index].substr(0, dot);
+
+		cout <<"Command: '" <<command_conditionless << "' . '" << condition << "'" << endl;
+	}
+	else
+	{
+		command_conditionless = words[index];
+		cout << "Command: '" << command_conditionless << "'" << endl;
+	}
+			
+	// recognize command
+
+	int number_arguments = word_count - index - 1;
+
+	if (command_conditionless == "ADD")
+	{
+		cout << "mame add " << endl;
+		if (number_arguments != 3)
+		{
+			wrong_arguments("ADD", number_arguments, line_number);
+		}
+	}
+	else if (command_conditionless == "OUT")
+	{
+		cout << "mame out " << endl;
+		if (number_arguments != 1)
+		{
+			wrong_arguments("OUT", number_arguments, line_number);
+		}
+	}
+	else if (command_conditionless == "JMP")
+	{
+		cout << "mame jmp " << endl;
+		if (number_arguments != 1)
+		{
+			wrong_arguments("JMP", number_arguments, line_number);
+		}
+	}
+	else if (command_conditionless.substr(0,3) == "CMP")
+	{
+		cout << "mame nejakej CMP" << endl;
+		if (number_arguments != 4)
+		{
+			wrong_arguments("CMP", number_arguments, line_number);
+		}
+	}
+	
+
+
+
+
+
+
+
+
+
 	// actually parsing
 	// ...
 
 	// no returning by forcing std::move(), we are letting the compiler do what he thinks is best
-	return parsed;
+	return instr_JMP("TU",1);
 }
 
 void read_input(file_resource& in, vector<instruction>& out_vec)
 {
 	string line;
+	int line_num = 1;
 
 	// some kind of while cycle
 	while (getline(in.ifs, line))
 	{
-		out_vec.push_back(parse_line(line));
+		out_vec.push_back(parse_line(line, line_num));
+		line_num++;
 	}
 }
 
@@ -86,6 +270,7 @@ int main(int argc, char**argv)
 
 
 	// TEST AREA
+	/*
 	vector< unique_ptr<instruction> > vec;
 
 	vec.push_back(move(make_unique<instr_DIV>(1, floating, 10, 5, 6)));
@@ -180,6 +365,9 @@ int main(int argc, char**argv)
 	cout << "GT " << (bool)prg.P_register->at(106) << " " << (bool)prg.P_register->at(107) << endl;
 	cout << "LE " << (bool)prg.P_register->at(108) << " " << (bool)prg.P_register->at(109) << endl;
 	cout << "LT " << (bool)prg.P_register->at(110) << " " << (bool)prg.P_register->at(111) << endl;
+
+	*/
+
 	return 0;
 }
 
